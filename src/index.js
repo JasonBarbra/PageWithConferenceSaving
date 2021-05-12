bae7c3bc1451e57f08a82bb26ba2e1ad23950b1d
Jason <adaglo524@gmail.com>
2021-05-12 21:00:40 +0200

    new wbienars

--- src/index.js
+++ src/index.js
@@ -8,24 +8,40 @@ const logout = document.getElementById("logout");
 const logoutpart = document.querySelector(".sect-logout");
 dataButton = document.getElementById('data1');
 const error = document.querySelector('.error');
-let jason;
+const span2 = document.getElementById("span2");
+let jason=[];
+let jason2=[];
 let Jsonindex=0;
+let Jsonindex2=0;
 let personCounter=0;
+let personCounter2=0;
 auth.onAuthStateChanged(user =>{
     if(user){
         jason = [];
+        jason2 = [];
         Jsonindex=0;
+        Jsonindex2=0;
         loginpart.classList.add('d-none');
         logoutpart.classList.remove('d-none');
-        db.collection('event1').orderBy('send_at','desc').get().then((snapshot) =>{
+        counter.classList.remove('d-none');
+        db.collection('event3').orderBy('send_at','desc').get().then((snapshot) =>{
             personCounter=snapshot.docs.length;
-            counter.classList.remove('d-none');
             counter.querySelector('span').textContent = `${personCounter}`;
             snapshot.docs.forEach(doc =>{
                 jason.push(doc.data());
                 const when = dateFns.format( jason[Jsonindex].send_at.toDate(), 'Do.MMMM.YYYY');
                 jason[Jsonindex].send_at = when;
                 Jsonindex+=1;
+            });
+         });
+         db.collection('event2').orderBy('send_at','desc').get().then((snapshot) =>{
+            personCounter2=snapshot.docs.length;
+            span2.textContent = `${personCounter2}`;
+            snapshot.docs.forEach(doc =>{
+                jason2.push(doc.data());
+                const when = dateFns.format( jason2[Jsonindex2].send_at.toDate(), 'Do.MMMM.YYYY');
+                jason2[Jsonindex2].send_at = when;
+                Jsonindex2+=1;
             });
          });
     }
@@ -36,6 +52,7 @@ auth.onAuthStateChanged(user =>{
         counter.classList.add('d-none');
         logoutpart.classList.add('d-none');
         jason=[];
+        jason2=[];
     }
  })
 
